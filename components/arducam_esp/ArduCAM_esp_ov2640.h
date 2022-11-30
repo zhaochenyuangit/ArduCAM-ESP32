820ce4559473ed32b8c67e04d2d035740c9a181e
SelfTide <94939562+SelfTide@users.noreply.github.com>
2022-11-29 21:39:46 -0600

    added C wrapper functions for most functions

--- components/arducam_esp/ArduCAM_esp_ov2640.h
+++ components/arducam_esp/ArduCAM_esp_ov2640.h
@@ -1,5 +1,22 @@
+#ifndef ARDUCAM_OV2640
+#define ARDUCAM_OV2640
+
 #include "ArduCAM_esp.h"
 #include "ov2640_regs.h"
+
+#define USING_C
+
+#ifdef USING_C
+
+
+
+#endif
+
+#define ARDUCHIP_FRAMES			0x01
+#define VSYNC_LEVEL_MASK   		0x02
+#define FRAMES_NUM 				0x04
+
+#ifdef __cplusplus
 class ArduCAM_OV2640 : public ArduCAM
 {
 public:
@@ -12,3 +29,36 @@ public:
     void set_Contrast(arducam_contrast_t Contrast);
     void set_Special_effects(arducam_special_effects_t Special_effect);
 };
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	typedef struct ArduCAM_OV2640_C ArduCAM_OV2640_C;
+
+	ArduCAM_OV2640_C *myCAM;
+
+	// wrapper functions for C compatibility, should this be moved to header file?
+	extern void ArduCAM_set_bit(uint8_t addr, uint8_t bit);
+	extern void *ArduCAM_OV2640_init(gpio_num_t CS_PIN);
+	extern void ArduCAM_write_reg(uint8_t addr, uint8_t data);
+	extern uint8_t ArduCAM_read_reg(uint8_t addr);
+	extern byte ArduCAM_wrSensorReg8_8(int regID, int regDat);
+	extern byte ArduCAM_rdSensorReg8_8(uint8_t regID, uint8_t *regDat);
+	extern void ArduCAM_flush_fifo();
+	extern void ArduCAM_clear_fifo_flag();
+	extern void ArduCAM_start_capture();
+	extern uint8_t ArduCAM_get_bit(uint8_t addr, uint8_t bit);
+	extern uint32_t ArduCAM_read_fifo_length();
+	extern void ArduCAM_CS_LOW();
+	extern void ArduCAM_CS_HIGH();
+	extern void ArduCAM_OV2640_InitCAM();
+	extern void ArduCAM_set_format(byte fmt);
+	extern void ArduCAM_OV2640_set_JPEG_size(int size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
